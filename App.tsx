dfef911696af7f8a8ee33830bf6ddccb43309dff
zearn-app <zoroz0673@gmail.com>
2026-03-27 12:31:22 +0530

    Update App.tsxgj

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { ErrorBoundary } from './components/ErrorBoundary';
 import { NotificationProvider } from './components/NotificationSystem';
 
 // Pages
+import HistoryPage from './pages/History';
 import Onboarding from './pages/Onboarding';
 import Login from './pages/Login';
 import Home from './pages/Home';
