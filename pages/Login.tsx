a55518b4066c18991bbdf36c93e9a0c96b57a7cc
zearn-app <zoroz0673@gmail.com>
2026-03-27 16:15:28 +0530

    Update Login.tsxDy

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -64,7 +64,134 @@ const Login: React.FC = () => {
     "Gadchiroli", "Ratnagiri", "Sindhudurg", "Palghar",
     "Raigad", "Dhule", "Nandurbar"
   ]
+"Gujarat": [
+    "Ahmedabad","Amreli","Anand","Aravalli","Banaskantha","Bharuch",
+    "Bhavnagar","Botad","Chhota Udaipur","Dahod","Dang","Devbhoomi Dwarka",
+    "Gandhinagar","Gir Somnath","Jamnagar","Junagadh","Kheda",
+    "Kutch","Mahisagar","Mehsana","Morbi","Narmada","Navsari",
+    "Panchmahal","Patan","Porbandar","Rajkot","Sabarkantha",
+    "Surat","Surendranagar","Tapi","Vadodara","Valsad"
+  ],
 
+  "Rajasthan": [
+    "Ajmer","Alwar","Banswara","Baran","Barmer","Bharatpur","Bhilwara",
+    "Bikaner","Bundi","Chittorgarh","Churu","Dausa","Dholpur","Dungarpur",
+    "Hanumangarh","Jaipur","Jaisalmer","Jalore","Jhalawar","Jhunjhunu",
+    "Jodhpur","Karauli","Kota","Nagaur","Pali","Pratapgarh",
+    "Rajsamand","Sawai Madhopur","Sikar","Sirohi","Sri Ganganagar",
+    "Tonk","Udaipur"
+  ],
+
+  "Uttar Pradesh": [
+    "Agra","Aligarh","Allahabad","Ambedkar Nagar","Amethi","Amroha",
+    "Auraiya","Azamgarh","Baghpat","Bahraich","Ballia","Balrampur",
+    "Banda","Barabanki","Bareilly","Basti","Bhadohi","Bijnor",
+    "Budaun","Bulandshahr","Chandauli","Chitrakoot","Deoria","Etah",
+    "Etawah","Faizabad","Farrukhabad","Fatehpur","Firozabad",
+    "Gautam Buddha Nagar","Ghaziabad","Ghazipur","Gonda","Gorakhpur",
+    "Hamirpur","Hapur","Hardoi","Hathras","Jalaun","Jaunpur",
+    "Jhansi","Kannauj","Kanpur Dehat","Kanpur Nagar","Kasganj",
+    "Kaushambi","Kheri","Kushinagar","Lalitpur","Lucknow",
+    "Maharajganj","Mahoba","Mainpuri","Mathura","Mau","Meerut",
+    "Mirzapur","Moradabad","Muzaffarnagar","Pilibhit","Pratapgarh",
+    "Raebareli","Rampur","Saharanpur","Sambhal","Sant Kabir Nagar",
+    "Shahjahanpur","Shamli","Shravasti","Siddharthnagar","Sitapur",
+    "Sonbhadra","Sultanpur","Unnao","Varanasi"
+  ],
+
+  "Madhya Pradesh": [
+    "Agar Malwa","Alirajpur","Anuppur","Ashoknagar","Balaghat",
+    "Barwani","Betul","Bhind","Bhopal","Burhanpur","Chhatarpur",
+    "Chhindwara","Damoh","Datia","Dewas","Dhar","Dindori",
+    "Guna","Gwalior","Harda","Hoshangabad","Indore","Jabalpur",
+    "Jhabua","Katni","Khandwa","Khargone","Mandla","Mandsaur",
+    "Morena","Narsinghpur","Neemuch","Panna","Raisen","Rajgarh",
+    "Ratlam","Rewa","Sagar","Satna","Sehore","Seoni","Shahdol",
+    "Shajapur","Sheopur","Shivpuri","Sidhi","Singrauli",
+    "Tikamgarh","Ujjain","Umaria","Vidisha"
+  ],
+
+  "West Bengal": [
+    "Alipurduar","Bankura","Birbhum","Cooch Behar","Dakshin Dinajpur",
+    "Darjeeling","Hooghly","Howrah","Jalpaiguri","Jhargram",
+    "Kalimpong","Kolkata","Malda","Murshidabad","Nadia",
+    "North 24 Parganas","Paschim Bardhaman","Paschim Medinipur",
+    "Purba Bardhaman","Purba Medinipur","Purulia",
+    "South 24 Parganas","Uttar Dinajpur"
+  ],
+
+  "Punjab": [
+    "Amritsar","Barnala","Bathinda","Faridkot","Fatehgarh Sahib",
+    "Fazilka","Firozpur","Gurdaspur","Hoshiarpur","Jalandhar",
+    "Kapurthala","Ludhiana","Mansa","Moga","Muktsar",
+    "Nawanshahr","Pathankot","Patiala","Rupnagar","Sangrur",
+    "Tarn Taran"
+  ],
+
+  "Haryana": [
+    "Ambala","Bhiwani","Charkhi Dadri","Faridabad","Fatehabad",
+    "Gurgaon","Hisar","Jhajjar","Jind","Kaithal","Karnal",
+    "Kurukshetra","Mahendragarh","Nuh","Palwal","Panchkula",
+    "Panipat","Rewari","Rohtak","Sirsa","Sonipat","Yamunanagar"
+  ],
+
+  "Odisha": [
+    "Angul","Balangir","Balasore","Bargarh","Bhadrak","Boudh",
+    "Cuttack","Deogarh","Dhenkanal","Gajapati","Ganjam",
+    "Jagatsinghpur","Jajpur","Jharsuguda","Kalahandi",
+    "Kandhamal","Kendrapara","Kendujhar","Khordha","Koraput",
+    "Malkangiri","Mayurbhanj","Nabarangpur","Nayagarh",
+    "Nuapada","Puri","Rayagada","Sambalpur","Subarnapur",
+    "Sundargarh"
+  ],
+
+  "Jharkhand": [
+    "Bokaro","Chatra","Deoghar","Dhanbad","Dumka","East Singhbhum",
+    "Garhwa","Giridih","Godda","Gumla","Hazaribagh",
+    "Jamtara","Khunti","Koderma","Latehar","Lohardaga",
+    "Pakur","Palamu","Ramgarh","Ranchi","Sahebganj",
+    "Seraikela Kharsawan","Simdega","West Singhbhum"
+  ],
+
+  "Chhattisgarh": [
+    "Balod","Baloda Bazar","Balrampur","Bastar","Bemetara",
+    "Bijapur","Bilaspur","Dantewada","Dhamtari","Durg",
+    "Gariaband","Janjgir Champa","Jashpur","Kabirdham",
+    "Kanker","Kondagaon","Korba","Korea","Mahasamund",
+    "Mungeli","Narayanpur","Raigarh","Raipur","Rajnandgaon",
+    "Sukma","Surajpur","Surguja"
+  ],
+
+  "Uttarakhand": [
+    "Almora","Bageshwar","Chamoli","Champawat","Dehradun",
+    "Haridwar","Nainital","Pauri Garhwal","Pithoragarh",
+    "Rudraprayag","Tehri Garhwal","Udham Singh Nagar","Uttarkashi"
+  ],
+
+  "Himachal Pradesh": [
+    "Bilaspur","Chamba","Hamirpur","Kangra","Kinnaur",
+    "Kullu","Lahaul and Spiti","Mandi","Shimla","Sirmaur",
+    "Solan","Una"
+  ],
+
+  "Goa": [
+    "North Goa","South Goa"
+  ],
+
+  "Sikkim": [
+    "East Sikkim","North Sikkim","South Sikkim","West Sikkim"
+  ],
+
+  "Telangana": [
+    "Adilabad","Bhadradri Kothagudem","Hyderabad","Jagtial",
+    "Jangaon","Jayashankar Bhupalpally","Jogulamba Gadwal",
+    "Kamareddy","Karimnagar","Khammam","Komaram Bheem",
+    "Mahabubabad","Mahabubnagar","Mancherial","Medak",
+    "Medchal","Nagarkurnool","Nalgonda","Nirmal","Nizamabad",
+    "Peddapalli","Rajanna Sircilla","Rangareddy","Sangareddy",
+    "Siddipet","Suryapet","Vikarabad","Wanaparthy",
+    "Warangal Rural","Warangal Urban","Yadadri Bhuvanagiri"
+  ]
   // 👉 Continue for all states...
 };
   const [formData, setFormData] = useState({
