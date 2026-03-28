3bd8d05847f0828f553372b201a970cc3e326ffd
zearn-app <zoroz0673@gmail.com>
2026-03-28 16:36:27 +0530

    Create DisableBack.tsx

---
+++ DisableBack.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+const DisableBack = () => {
+  useEffect(() => {
+    window.history.pushState(null, "", window.location.href);
+
+    const handlePopState = () => {
+      window.history.pushState(null, "", window.location.href);
+    };
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  return null;
+};
+
+export default DisableBack;
